eee02ba64ab3083679063382fc6998507ed0d88f
lontten <lontten@163.com>
2026-01-07 18:16:03 +0800

    添加直接返回[]byte的未封装方法

--- netutil/net.go
+++ netutil/net.go
@@ -55,6 +55,18 @@ func PostJson[T any](targetURL string, data any) (T, error) {
 }
 func PostJsonBase[T any](targetURL string, data any) (int, T, error) {
 	var result T
+	code, body, err := PostJsonNative(targetURL, data)
+	if err != nil {
+		return 0, result, err
+	}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return 0, result, fmt.Errorf("响应体 JSON 反序列化失败: %w", err)
+	}
+	return code, result, nil
+}
+
+func PostJsonNative(targetURL string, data any) (int, []byte, error) {
 	var jsonBody []byte
 	var err error
 
@@ -63,33 +75,37 @@ func PostJsonBase[T any](targetURL string, data any) (int, T, error) {
 	} else {
 		jsonBody, err = json.Marshal(data)
 		if err != nil {
-			return 0, result, fmt.Errorf("请求体 JSON 序列化失败: %w", err)
+			return 0, []byte{}, fmt.Errorf("请求体 JSON 序列化失败: %w", err)
 		}
 	}
-
 	resp, err := http.Post(targetURL, "application/json", bytes.NewBuffer(jsonBody))
 	if err != nil {
-		return 0, result, fmt.Errorf("发送 PostJson 请求失败: %w", err)
+		return 0, []byte{}, fmt.Errorf("发送 PostJson 请求失败: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
 		// 读取错误响应体（可选，用于更详细的错误信息）
 		errBody, _ := io.ReadAll(resp.Body)
-		return 0, result, fmt.Errorf("请求失败，状态码: %d，响应: %s", resp.StatusCode, string(errBody))
+		return 0, []byte{}, fmt.Errorf("请求失败，状态码: %d，响应: %s", resp.StatusCode, string(errBody))
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return 0, result, fmt.Errorf("读取响应体失败: %w", err)
+		return 0, []byte{}, fmt.Errorf("读取响应体失败: %w", err)
 	}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return 0, result, fmt.Errorf("响应体 JSON 反序列化失败: %w", err)
-	}
-	return resp.StatusCode, result, nil
+	return resp.StatusCode, body, nil
 }
-
+func PostJsonNativeOk(targetURL string, data any) ([]byte, error) {
+	statusCode, result, err := PostJsonNative(targetURL, data)
+	if err != nil {
+		return result, err
+	}
+	if statusCode != http.StatusOK {
+		return result, fmt.Errorf("请求失败，状态码: %d", statusCode)
+	}
+	return result, nil
+}
 func PostForm[T any](targetURL string, data url.Values) (T, error) {
 	statusCode, result, err := PostFormBase[T](targetURL, data)
 	if err != nil {
